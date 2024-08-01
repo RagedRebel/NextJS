013bed849a8b1f3bbda301935e068015f64c7652
RagedRebel <reubenav1305@gmail.com>
2024-08-01 23:24:55 +0530

    build: some progress on navbar

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
       <NavBar/>
+        {children}
+      
       </body>
     </html>
   );
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
 const NavBar = () => {
+    
   return (
-    <div>NavBar</div>
+    <>
+    <div className="bg-sky-600 mb-4 p-2 h-10 ">
+      
+        <ul className="flex flex-row-reverse   gap-5 ">
+            <button>Sign Out</button>
+            <button>Account</button>
+            <button>Cart</button>
+            <button>Home</button>
+            <h1>TheNavBar</h1>
+
+
+        </ul>
+        
+        
+    </div>
+    </>
   )
 }
 
