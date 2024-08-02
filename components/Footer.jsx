14b24c22beb4f7caaed9baee0185840728a3fa87
RagedRebel <reubenav1305@gmail.com>
2024-08-02 23:10:47 +0530

    build: added logo to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import Logo from '../public/saturn.png'
 import Link from 'next/link'
 
 const Footer = () => {
@@ -6,7 +8,15 @@ const Footer = () => {
   return (
     <>
     <div className="bg-sky-600  p-2 h-10 absolute bottom-0 flex justify-between items-center place-content-end w-full">
-        <h1 className='font-bold font-mono'>TheFooter</h1>
+    <div className='flex items-center space-x-4'> 
+        <Image
+      src={Logo}
+      alt="logo"
+      height={30}
+      width={30}  
+    />
+    <h1 className='font-bold font-mono'>TheFooter</h1>
+    </div>
       
         <ul className="space-x-5">
             <Link href="/cart">
