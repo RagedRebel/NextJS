b9844543ec27cc920ac33d78e90d1d5e830fa75f
RagedRebel <reubenav1305@gmail.com>
2024-08-03 12:20:48 +0530

    build:completed home page

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import Card from '@components/Card'
+    import React from 'react'
+    import Card from '@components/Card'
+    import Car from '../../public/car.png'
+    import Laptop from '../../public/laptop.png'
+    import Mobile from '../../public/mobile.png'
 
-const home = () => {
-  return (
-    <div><Card/></div>
-  )
-}
 
-export default home
+    const products=[
+        {name:'Car',
+            price:40000,
+            img:Car,      
+        
+            },
+        {
+            name:'Laptop',
+            price:'600',
+            img:Laptop,
+
+            },
+            { name:'Mobile',
+                price:'500',
+                img:Mobile,
+
+            }
+        
+    ]
+
+    const home = () => {
+
+    return (
+        <div>
+            {products.map((product)=>(
+            <Card product={product}/>
+            ))}
+
+            </div>
+    )
+    }
+
+    export default home
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import Logo from '../public/saturn.png'
+
 import Button from '@components/My_Button'
 
-const card = () => {
+const card = ({product}) => {
+  
   return (
   <div className="border-2 bg-zinc-300 border-slate-100 rounded-md h-20 p-2 flex justify-between items-center m-2 hover:border-sky-600">
     <div className='flex justify-center items-center'>
     <Image
-      src={Logo}
-      alt="logo"
+      src={product.img}
+      alt={product.name}
       height={50}
       width={50}  
       className='m-3'
     />
-    <h1 className='text-black font-black m-2'>Product 1</h1>
-    <p className='text-black m-2 font-semibold'>Price:$</p>
+    <h1 className='text-black font-black m-2'>{product.name}</h1>
+    <p className='text-black m-2 font-semibold'>Price:${product.price}</p>
     </div>
     <Button/>
   </div>
